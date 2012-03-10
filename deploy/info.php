4a9a4ac0f8565d6e15acd9a17c230570141bfeec
Peter Baldwin <pbaldwin@clearfoundation.com>
2012-03-10 01:03:41 +0000

    - Removed debug statement (ahem)

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'web_proxy';
-$app['version'] = '1.0.4';
+$app['version'] = '1.0.6';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
@@ -41,6 +41,7 @@ $app['requires'] = array(
 $app['core_requires'] = array(
     'app-network-core',
     'app-firewall-core',
+    'app-samba-core >= 1:1.0.6',
     'app-web-proxy-plugin-core',
     'csplugin-filewatch',
     'squid >= 3.1.10',
