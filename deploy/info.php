996b69d7a0d150cf6211498e615b3ea9a5021c54
Peter Baldwin <pbaldwin@clearfoundation.com>
2013-03-01 01:54:08 +0000

    - Created 6.4.0 Beta 2 snapshot

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'web_proxy';
-$app['version'] = '1.4.15';
+$app['version'] = '1.4.20';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
