fb892cd034d4b0d7fb5044edb41a2a1adddc1bfd
pcbaldwin <pbaldwin@clearfoundation.com>
2014-06-24 14:56:14 -0400

    Prepped for 6.6.0 Beta 2

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'web_proxy';
-$app['version'] = '1.6.3';
+$app['version'] = '1.6.5';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
