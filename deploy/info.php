cdc304196e808ba8997bcab8f71c95154eb46a63
pcbaldwin <pbaldwin@clearfoundation.com>
2014-10-09 18:44:41 -0400

    Prepping for snapshot build

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'web_proxy';
-$app['version'] = '2.0.0';
+$app['version'] = '2.0.1';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
--- packaging/app-web-proxy.spec
+++ packaging/app-web-proxy.spec
@@ -1,7 +1,7 @@
 
 Name: app-web-proxy
 Epoch: 1
-Version: 2.0.0
+Version: 2.0.1
 Release: 1%{dist}
 Summary: Web Proxy
 License: GPLv3
