41a7111190b66f81bb770120cca45964769f1fc9
pcbaldwin <pbaldwin@clearfoundation.com>
2015-11-23 15:28:24 -0500

    Forced IPv4 DNS lookups to be done first [tracker #6361]

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'web_proxy';
-$app['version'] = '2.1.17';
+$app['version'] = '2.2.0';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
